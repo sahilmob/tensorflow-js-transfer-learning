136f6132fcf48a14abb624e1cdbcc53feccd97de
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2024-05-13 19:21:57 +0300

    create basic markup

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,17 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <p>Awaiting TF.js</p>
+      <video autoPlay />
+      <button>Enable Webcam</button>
+      <button data-hot="0">Gather Class 1</button>
+      <button data-hot="1">Gather Class 2</button>
+      <button>Train & Predict</button>
+      <button>Reset</button>
+    </>
+  );
 }
 
 export default App;
